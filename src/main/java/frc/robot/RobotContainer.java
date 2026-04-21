3d9cf9d734c6f3764da2c1f437498725558615d9
baihu <baihu3210@gmail.com>
2026-04-21 19:09:53 +0800

    Add initial project structure and configuration files for 2026 Robot Code

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+// import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+public class RobotContainer {
+  	// private final CommandXboxController controller = new CommandXboxController(0);
+
+  	public RobotContainer() {
+    	configureBindings();
+  	}
+
+  	private void configureBindings() {
+  	}
+
+  	public Command getAutonomousCommand() {
+    	return null;
+	}
+}
