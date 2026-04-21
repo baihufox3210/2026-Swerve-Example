6aed2680a35b6e998073f2594180b446409a5962
baihu <baihu3210@gmail.com>
2026-04-21 20:45:58 +0800

    Refactor RobotContainer to initialize Drivetrain and set default command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,19 +1,24 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
-// import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Commands.Drivetrain.Drive;
+import frc.robot.subsystems.Drivetrain.Drivetrain;
 
 public class RobotContainer {
-  	// private final CommandXboxController controller = new CommandXboxController(0);
+  	private final CommandXboxController controller = new CommandXboxController(0);
+
+	private final Drivetrain drivetrain = Drivetrain.getInstance();
 
   	public RobotContainer() {
+		drivetrain.setDefaultCommand(new Drive(controller));
     	configureBindings();
   	}
 
-  	private void configureBindings() {
-  	}
+  	private void configureBindings() {}
 
   	public Command getAutonomousCommand() {
-    	return null;
+    	return Commands.print("No autonomous command configured");
 	}
 }
